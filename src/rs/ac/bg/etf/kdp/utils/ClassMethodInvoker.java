c3faf22bbfa4b8d3a8d27e3e32f38c782bc33680
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2023-01-30 00:14:31 +0100

    Creates a runnable object that invokes a method on an instance of the specified class with given arguments

---
+++ src/rs/ac/bg/etf/kdp/utils/ClassMethodInvoker.java
@@ -0,0 +1,48 @@
+package rs.ac.bg.etf.kdp.utils;
+
+import java.lang.reflect.*;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ClassMethodInvoker extends Thread {
+    final Class<?> threadClass;
+    private final Object instance;
+    private final Method method;
+    private final Object[] methodArgs;
+
+    public ClassMethodInvoker(String className, Object[] ctorArgs,
+                              String methodName, Object[] methodArgs)
+            throws ReflectiveOperationException {
+        super(className+"::"+methodName);
+        threadClass = Class.forName(className);
+        instance = getConstructor(ctorArgs);
+        method = getMethod(methodName, methodArgs);
+        this.methodArgs = methodArgs;
+    }
+
+    @Override
+    public void run() {
+        try {
+            method.invoke(instance, methodArgs);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private Object getConstructor(Object[] ctorArgs)
+            throws ReflectiveOperationException {
+        final var ctorArgTypes = Arrays.stream(ctorArgs)
+                .map(Object::getClass)
+                .toArray(Class[]::new);
+        return threadClass
+                .getConstructor(ctorArgTypes)
+                .newInstance(ctorArgs);
+    }
+
+    private Method getMethod(String methodName, Object[] methodArgs)
+            throws ReflectiveOperationException {
+        final var methodParamTypes = Stream.of(methodArgs)
+                .map(Object::getClass).toArray(Class[]::new);
+        return threadClass.getMethod(methodName, methodParamTypes);
+    }
+}
