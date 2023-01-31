6e0405311f43960b2850f0700c734be9e46b9273
db200260 <djumicbranislav@outlook.com>
2023-01-31 12:52:24 +0100

    Probably fixed

--- src/rs/ac/bg/etf/kdp/core/ProcessWorker.java
+++ src/rs/ac/bg/etf/kdp/core/ProcessWorker.java
@@ -9,20 +9,23 @@ import java.util.Date;
 import java.util.UUID;
 
 public class ProcessWorker implements IRMIProcessWorker {
+    static {
+        System.setProperty("sun.rmi.transport.tcp.responseTimeout", "10000");
+    }
     private IRMICentralServer server = null;
     private static final String format = "yyyy-MM-dd HH:mm:ss";
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat(format);
 
     @Override
     public void ping() {
-        System.out.printf("Ping from server at %s", dateFormat.format(new Date()));
+        System.out.printf("Ping from server at %s!\n", dateFormat.format(new Date()));
     }
 
     private void connect(String host, int port) {
         try {
             UnicastRemoteObject.exportObject(this, 0);
             Registry registry = LocateRegistry.getRegistry(host, port);
-            server = (IRMICentralServer) registry.lookup("/LindaServer");
+            server = (IRMICentralServer) registry.lookup("/CentralServer");
             server.registerWorker(UUID.randomUUID(), this);
 
         } catch (RemoteException | NotBoundException e) {
