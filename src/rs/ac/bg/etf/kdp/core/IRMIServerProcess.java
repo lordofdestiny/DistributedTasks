b207825a665a8fd64f68b9474d0f69a1c2a63f2c
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2023-01-31 23:46:46 +0100

    Renamed files in order to achive more uniform naming across the codebase

--- src/rs/ac/bg/etf/kdp/core/IRMIServerProcess.java
+++ src/rs/ac/bg/etf/kdp/core/IRMIServerProcess.java
@@ -4,7 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.UUID;
 
-public interface IRMICentralServer extends Remote {
-    void registerWorker(UUID id , IRMIProcessWorker worker) throws RemoteException;
+public interface IRMIServerProcess extends Remote {
+    void registerWorker(UUID id , IRMIWorkerProcess worker) throws RemoteException;
     void ping(UUID id) throws RemoteException;
 }
