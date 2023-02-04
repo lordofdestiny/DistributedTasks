7e6ea0b3f820c09c9c73ff0d5e9de8743b59f63b
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2023-02-04 04:37:06 +0100

    Moved WorkerRecord to a separate file

--- src/rs/ac/bg/etf/kdp/core/ServerProcess.java
+++ src/rs/ac/bg/etf/kdp/core/ServerProcess.java
@@ -5,7 +5,6 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -23,41 +22,6 @@ public class ServerProcess
 
 	static {
         Configuration.load();
-    }
-
-    private static class WorkerRecord {
-        public UUID id;
-        public IWorkerServer handle;
-        private boolean online;
-
-        WorkerRecord(UUID id, IWorkerServer worker) {
-            this.id = id;
-            this.handle = worker;
-            this.online = true;
-        }
-
-        public boolean isOnline() {
-            return online;
-        }
-
-        public void setOnline() {
-            this.online = true;
-        }
-
-        public void setOffline() {
-            this.online = false;
-        }
-
-        public Optional<Long> ping() {
-            try {
-                final var start = System.currentTimeMillis();
-                handle.ping();
-                final var end = System.currentTimeMillis();
-                return Optional.of(end - start);
-            } catch (RemoteException e) {
-                return Optional.empty();
-            }
-        }
     }
 
     private final Map<UUID, WorkerRecord> registeredWorkers = new ConcurrentHashMap<>();
