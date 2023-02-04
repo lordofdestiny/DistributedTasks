ce72fe2f59d2084c1841a7f153098bb941dc6785
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2023-02-04 05:05:40 +0100

    Code cleanup

--- src/rs/ac/bg/etf/kdp/core/ClientProcess.java
+++ src/rs/ac/bg/etf/kdp/core/ClientProcess.java
@@ -2,27 +2,26 @@ package rs.ac.bg.etf.kdp.core;
 
 import java.nio.file.Path;
 import java.rmi.NoSuchObjectException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import rs.ac.bg.etf.kdp.gui.client.ServerUnavailableException;
+import rs.ac.bg.etf.kdp.utils.Configuration;
 import rs.ac.bg.etf.kdp.utils.ConnectionInfo;
 import rs.ac.bg.etf.kdp.utils.ConnectionListener;
 import rs.ac.bg.etf.kdp.utils.ConnectionMonitor;
+import rs.ac.bg.etf.kdp.utils.ConnectionProvider;
 import rs.ac.bg.etf.kdp.utils.TransferListener;
 
 public class ClientProcess implements IClientServer {
-	private static final String SERVER_ROUTE = System.getProperty("server.route");
-	private static final int SERVER_PORT = Integer.parseInt(System.getProperty("server.port"));
 	private ConnectionMonitor monitor = null;
 	private final ConnectionInfo conectionInfo;
 	private final UUID uuid;
 	private IServerClient server = null;
-	
+
 	public ClientProcess(UUID uuid, ConnectionInfo info) {
 		this.uuid = uuid;
 		this.conectionInfo = info;
@@ -33,12 +32,13 @@ public class ClientProcess implements IClientServer {
 	}
 
 	public boolean connectToServer(List<ConnectionListener> listeners) throws ServerUnavailableException {
+		server = ConnectionProvider.connect(conectionInfo, IServerClient.class)
+				.orElseThrow(ServerUnavailableException::new);
+
 		try {
 			UnicastRemoteObject.exportObject(this, 0);
-			Registry registry = LocateRegistry.getRegistry(conectionInfo.getIp(), conectionInfo.getPort());
-			server = (IServerClient) registry.lookup(SERVER_ROUTE);
 			server.register(uuid, this);
-		} catch (Exception e) {
+		} catch (RemoteException e) {
 			throw new ServerUnavailableException();
 		}
 		final var that = this;
@@ -60,18 +60,28 @@ public class ClientProcess implements IClientServer {
 	public ConnectionMonitor getConnectionMonitor() {
 		return monitor;
 	}
-	
 
 	public void sendJob(Path zipFile, TransferListener cb) {
-		
+
 	}
-	
-	public void sendJob() {
-		
+
+	@SuppressWarnings("unused")
+	private class JobSender extends Thread {
+		public JobSender(Path zip, TransferListener cb) {
+
+		}
+
+		@Override
+		public void run() {
+			// ask server to register a new job
+			// send job to server
+
+		}
 	}
 
 	public static void main(String[] args) {
-		ClientProcess cp = new ClientProcess(UUID.randomUUID(), new ConnectionInfo("localhost", SERVER_PORT));
+		final var cinfo = new ConnectionInfo("localhost", Configuration.SERVER_PORT);
+		ClientProcess cp = new ClientProcess(UUID.randomUUID(), cinfo);
 		final var listeners = new ArrayList<ConnectionListener>(1);
 		listeners.add(new ConnectionListener() {
 			@Override
@@ -111,20 +121,6 @@ public class ClientProcess implements IClientServer {
 			System.err.println("Failed to connect to server!");
 			e.printStackTrace();
 			System.exit(0);
-		}
-	}
-
-	@SuppressWarnings("unused")
-	private class JobSender extends Thread {
-		public JobSender(Path zip, TransferListener cb) {
-			
-		}
-		
-		@Override
-		public void run() {
-			// ask server to register a new job
-			// send job to server
-						
 		}
 	}
 }
