1634172a11551162bdb9a4c32d0062895043a926
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2023-02-04 02:37:38 +0100

    Refactored to use IPingable instead of removed Pingable

--- src/rs/ac/bg/etf/kdp/core/IServerClient.java
+++ src/rs/ac/bg/etf/kdp/core/IServerClient.java
@@ -1,11 +1,13 @@
 package rs.ac.bg.etf.kdp.core;
 
-import rs.ac.bg.etf.kdp.utils.ConnectionMonitor;
-
-import java.rmi.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.UUID;
 
-public interface IServerClient extends ConnectionMonitor.Pingable, Remote {
+public interface IServerClient extends IPingable, IUUIDPingable, Remote {
     void register(UUID id, IClientServer client) throws RemoteException;
+
     void ping(UUID id) throws RemoteException;
+
+    void ping() throws RemoteException;
 }
--- src/rs/ac/bg/etf/kdp/core/IServerWorker.java
+++ src/rs/ac/bg/etf/kdp/core/IServerWorker.java
@@ -1,11 +1,9 @@
 package rs.ac.bg.etf.kdp.core;
 
-import rs.ac.bg.etf.kdp.utils.ConnectionMonitor;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.UUID;
 
-public interface IServerWorker extends ConnectionMonitor.Pingable, Remote {
+public interface IServerWorker extends IPingable, IUUIDPingable, Remote {
     void register(UUID id, IWorkerServer worker) throws RemoteException;
 }
