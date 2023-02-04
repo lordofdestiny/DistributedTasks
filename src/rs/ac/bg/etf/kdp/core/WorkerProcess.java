fdfa087fee2ecfa58b3c526bd4e0a8b4dd57b6ee
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2023-02-04 02:29:16 +0100

    Code reordering

--- src/rs/ac/bg/etf/kdp/core/WorkerProcess.java
+++ src/rs/ac/bg/etf/kdp/core/WorkerProcess.java
@@ -1,106 +1,114 @@
 package rs.ac.bg.etf.kdp.core;
 
-import rs.ac.bg.etf.kdp.utils.*;
-
-import java.rmi.*;
-import java.rmi.registry.*;
-import java.rmi.server.*;
+import java.rmi.NoSuchObjectException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.UUID;
+
+import rs.ac.bg.etf.kdp.utils.Configuration;
+import rs.ac.bg.etf.kdp.utils.ConnectionListener;
+import rs.ac.bg.etf.kdp.utils.ConnectionMonitor;
 
 public class WorkerProcess implements IWorkerServer {
-    static {
-        try {
-            PropertyLoader.loadConfiguration();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	static {
+		Configuration.load();
+	}
 
-    private static final int SERVER_PING_INTERVAL = Integer.parseInt(System.getProperty("worker.pingInterval"));
-    private static final String DATE_FORMAT_STR = "dd.MM.YYYY. HH:mm:ss";
-    private static final DateFormat df = new SimpleDateFormat(DATE_FORMAT_STR);
-    private static final String SERVER_ROUTE = System.getProperty("server.route");
-    private final UUID uuid = UUID.randomUUID();
-    private final String host;
-    private final int port;
-    private IServerWorker server = null;
-    private ConnectionMonitor connectionTracker;
+	private static final String DATE_FORMAT_STR = "dd.MM.YYYY. HH:mm:ss";
+	private static final DateFormat df = new SimpleDateFormat(DATE_FORMAT_STR);
+	private final UUID uuid = UUID.randomUUID();
+	private final String host;
+	private final int port;
+	private IServerWorker server = null;
+	private ConnectionMonitor connectionTracker;
 
-    public WorkerProcess(String host, int port) {
-        this.host = host;
-        this.port = port;
+	public WorkerProcess(String host, int port) {
+		this.host = host;
+		this.port = port;
 
-    }
+	}
 
-    @Override
-    public void ping() {
-        if (connectionTracker != null && connectionTracker.connected()) {
-            System.out.printf("[%s]: Ping!\n", now());
-        }
-        try {
-            server.ping(uuid);
-        } catch (RemoteException e) {
-            System.err.printf("[%s]: Lost connection to server!", now());
-            System.err.println("Reconnecting...");
-        }
-    }
+	@Override
+	public void ping() {
+		if (connectionTracker != null && connectionTracker.connected()) {
+			System.out.printf("[%s]: Ping!\n", now());
+		}
+		try {
+			server.ping(uuid);
+		} catch (RemoteException e) {
+			System.err.printf("[%s]: Lost connection to server!", now());
+			System.err.println("Reconnecting...");
+		}
+	}
 
-    private boolean connectToServer() {
-        try {
-            UnicastRemoteObject.exportObject(this, 0);
-            Registry registry = LocateRegistry.getRegistry(host, port);
-            server = (IServerWorker) registry.lookup(SERVER_ROUTE);
-            server.register(uuid, this);
-        } catch (RemoteException | NotBoundException e) {
-            return false;
-        }
-        final var that = this;
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                UnicastRemoteObject.unexportObject(that, true);
-            } catch (NoSuchObjectException e) {
-                throw new RuntimeException(e);
-            }
-        }));
-        connectionTracker = new ConnectionMonitor(server, SERVER_PING_INTERVAL, uuid);
-        connectionTracker.addEventListener(new ConnectionListener(){
-            @Override
-            public void onPingComplete(long ping) {
-                System.out.printf("[%s]: Ping is %d ms\n", now(), ping);
-            }
-            @Override
-            public void onConnectionLost() {
-                System.err.println("Lost connection to server!");
-            }
-            @Override
-            public void onReconnecting() {
-                System.err.println("Reconnecting...");
-            }
-            @Override
-            public void onReconnected(long ping) {
-                System.out.printf("Reconnected, ping is %d ms\n", ping);
-            }
-            @Override
-            public void onReconnectionFailed() {
-                System.err.println("Could not reconnect to server! Exiting...");
-                System.exit(0);
-            }
-        });
-        connectionTracker.start();
-        return true;
-    }
+	private boolean connectToServer() {
+		try {
+			UnicastRemoteObject.exportObject(this, 0);
+			Registry registry = LocateRegistry.getRegistry(host, port);
+			server = (IServerWorker) registry.lookup(Configuration.SERVER_ROUTE);
+			server.register(uuid, this);
+		} catch (RemoteException | NotBoundException e) {
+			return false;
+		}
+		final var that = this;
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			try {
+				UnicastRemoteObject.unexportObject(that, true);
+			} catch (NoSuchObjectException e) {
+				throw new RuntimeException(e);
+			}
+		}));
+		connectionTracker = new ConnectionMonitor(server, Configuration.SERVER_PING_INTERVAL, uuid);
+		connectionTracker.addEventListener(new ConnectionListener() {
+			@Override
+			public void onConnected() {
+				System.out.println("Connected!!!");
+			}
+			@Override
+			public void onPingComplete(long ping) {
+				System.out.printf("[%s]: Ping is %d ms\n", now(), ping);
+			}
 
-    private static String now() {
-        return df.format(new Date());
-    }
+			@Override
+			public void onConnectionLost() {
+				System.err.println("Lost connection to server!");
+			}
 
-    public static void main(String[] args) {
-        WorkerProcess pw = new WorkerProcess("localhost", 8080);
-        if (!pw.connectToServer()) {
-            System.err.println("Failed to connect to server!");
-            System.exit(0);
-        }
-    }
+			@Override
+			public void onReconnecting() {
+				System.err.println("Reconnecting...");
+			}
+
+			@Override
+			public void onReconnected(long ping) {
+				System.out.printf("Reconnected, ping is %d ms\n", ping);
+			}
+
+			@Override
+			public void onReconnectionFailed() {
+				System.err.println("Could not reconnect to server! Exiting...");
+				System.exit(0);
+			}
+		});
+		connectionTracker.start();
+		return true;
+	}
+
+	private static String now() {
+		return df.format(new Date());
+	}
+
+	public static void main(String[] args) {
+		WorkerProcess pw = new WorkerProcess("localhost", 8080);
+		if (!pw.connectToServer()) {
+			System.err.println("Failed to connect to server!");
+			System.exit(0);
+		}
+	}
 }
