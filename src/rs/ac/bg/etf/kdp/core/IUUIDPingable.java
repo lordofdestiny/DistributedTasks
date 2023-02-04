e34c71d3b5717fa3adc78308503f476675737483
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2023-02-04 02:15:05 +0100

    Interface of an object that can be pinged by providing an UUID

---
+++ src/rs/ac/bg/etf/kdp/core/IUUIDPingable.java
@@ -0,0 +1,9 @@
+package rs.ac.bg.etf.kdp.core;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.UUID;
+
+public interface IUUIDPingable extends Remote {
+    void ping(UUID uuid) throws RemoteException;
+}
