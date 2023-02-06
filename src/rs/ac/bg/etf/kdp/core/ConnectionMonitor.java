7439c43f71e27797cf5516aa4df3e122babc8a16
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2023-02-06 22:29:40 +0100

    Refactored and fixed quit method

--- src/rs/ac/bg/etf/kdp/core/ConnectionMonitor.java
+++ src/rs/ac/bg/etf/kdp/core/ConnectionMonitor.java
@@ -16,48 +16,71 @@ public class ConnectionMonitor extends Thread {
 		super("ConnectionMonitor" + uuid);
 		this.server = server;
 		this.interval = pingInterval;
-		addEventListener(new DefaultListener());
+	}
+
+	private class KillException extends Exception {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 	}
 
 	@Override
 	public void run() {
 		boolean first = true;
 		while (true) {
+			if (Thread.interrupted())
+				return;
 			final var ping = IPingable.getPing(server);
-			if (ping.isPresent()) {
-				if (first) {
-					listeners.forEach(ConnectionListener::onConnected);
-					first = false;
-				}
-				// Ping successful
-				listeners.forEach(l -> l.onPingComplete(ping.get()));
+			if (Thread.interrupted())
+				return;
+			if (ping.isEmpty()) {
+				// Ping failed
+				setConnected(false);
+				listeners.forEach(ConnectionListener::onConnectionLost);
 				try {
-					// noinspection BusyWait
-					Thread.sleep(interval);
-				} catch (InterruptedException e) {
-					if (Thread.interrupted())
-						return;
+					reconnectToServer();
+				} catch (KillException e) {
+					return;
 				}
-				continue;
 			}
-			// Ping failed
-			listeners.forEach(ConnectionListener::onConnectionLost);
-			reconnectToServer();
+			if (first) {
+				setConnected(true);
+				listeners.forEach(ConnectionListener::onConnected);
+				first = false;
+			}
+			// Ping successful
+			lastOnlineTime = System.currentTimeMillis();
+			listeners.forEach(l -> l.onPingComplete(ping.get()));
+			try {
+				// noinspection BusyWait
+				Thread.sleep(interval);
+			} catch (InterruptedException e) {
+				return;
+			}
+
 		}
 	}
 
-	private void reconnectToServer() {
+	private void reconnectToServer() throws KillException {
 		while (System.currentTimeMillis() - lastOnlineTime < 60 * 1000) {
+			if (Thread.interrupted()) {
+				throw new KillException();
+			}
 			// Reconnecting
 			listeners.forEach(ConnectionListener::onReconnecting);
 			final var ping = IPingable.getPing(server);
 			if (ping.isPresent()) {
+				setConnected(true);
 				listeners.forEach(l -> l.onReconnected(ping.get()));
 				return;
 			}
 		}
 		// Reconnection failed
+		setConnected(false);
 		listeners.forEach(ConnectionListener::onReconnectionFailed);
+		quit();
 	}
 
 	private synchronized void setConnected(boolean connected) {
@@ -78,37 +101,5 @@ public class ConnectionMonitor extends Thread {
 
 	public void removeEventListener(ConnectionListener listener) {
 		listeners.remove(listener);
-	}
-
-	private class DefaultListener implements ConnectionListener {
-		@Override
-		public void onConnected() {
-			setConnected(true);
-		}
-
-		@Override
-		public void onPingComplete(long ping) {
-			lastOnlineTime = System.currentTimeMillis();
-		}
-
-		@Override
-		public void onConnectionLost() {
-			setConnected(false);
-		}
-
-		@Override
-		public void onReconnecting() {
-		}
-
-		@Override
-		public void onReconnected(long ping) {
-			setConnected(true);
-		}
-
-		@Override
-		public void onReconnectionFailed() {
-			setConnected(false);
-			quit();
-		}
 	}
 }
